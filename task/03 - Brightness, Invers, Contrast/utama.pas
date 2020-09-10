7a41245e1e3d154c1f247721198211ac6334e800
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-10 23:18:21 +0700

    feat: add label to show position of trackbar when changes happen

--- task/03 - Brightness, Invers, Contrast/utama.pas
+++ task/03 - Brightness, Invers, Contrast/utama.pas
@@ -10,9 +10,9 @@ uses
 
 type
 
-  { TForm1 }
+  { TFormUtama }
 
-  TForm1 = class(TForm)
+  TFormUtama = class(TForm)
     Bevel1: TBevel;
     ButtonLoad: TButton;
     ButtonSave: TButton;
@@ -35,22 +35,100 @@ type
     LabelThreshold: TLabel;
     OpenPictureDialog1: TOpenPictureDialog;
     SavePictureDialog1: TSavePictureDialog;
-    TrackBar1: TTrackBar;
-    TrackBar2: TTrackBar;
-    TrackBar3: TTrackBar;
-    TrackBar4: TTrackBar;
+    TrackBarBinary: TTrackBar;
+    TrackBarBrightness: TTrackBar;
+    TrackBarContrast: TTrackBar;
+    TrackBarG: TTrackBar;
+    procedure ButtonLoadClick(Sender: TObject);
+    procedure TrackBarGChange(Sender: TObject);
+    procedure TrackBarBinaryChange(Sender: TObject);
+    procedure TrackBarBrightnessChange(Sender: TObject);
+    procedure TrackBarContrastChange(Sender: TObject);
   private
+    procedure AssignBitmapBinary();
 
   public
 
   end;
 
 var
-  Form1: TForm1;
+  FormUtama: TFormUtama;
 
 implementation
 
 {$R *.lfm}
+
+{ TFormUtama }
+
+uses
+  windows;
+
+var
+  bitmapR, bitmapG, bitmapB : array[0..1000, 0..1000] of byte;
+  bitmapBinary : array[0..1000, 0..1000] of boolean;
+
+procedure TFormUtama.ButtonLoadClick(Sender: TObject);
+var
+  x, y : integer;
+begin
+  if OpenPictureDialog1.Execute then
+  begin
+    ImageSource.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+
+    for y := 0 to ImageSource.Height-1 do
+    begin
+      for x := 0 to ImageSource.Width-1 do
+      begin
+        bitmapR[x,y] := GetRValue(ImageSource.Canvas.Pixels[x,y]);
+        bitmapG[x,y] := GetGValue(ImageSource.Canvas.Pixels[x,y]);
+        bitmapB[x,y] := GetBValue(ImageSource.Canvas.Pixels[x,y]);
+      end;
+    end;
+  end;
+  AssignBitmapBinary();
+end;
+
+procedure TFormUtama.TrackBarGChange(Sender: TObject);
+begin
+  LabelG.Caption := IntToStr(TrackBarG.Position);
+end;
+
+procedure TFormUtama.TrackBarBinaryChange(Sender: TObject);
+begin
+  LabelThreshold.Caption := IntToStr(TrackBarBinary.Position);
+end;
+
+procedure TFormUtama.TrackBarBrightnessChange(Sender: TObject);
+begin
+  LabelBrightness.Caption := IntToStr(TrackBarBrightness.Position);
+end;
+
+procedure TFormUtama.TrackBarContrastChange(Sender: TObject);
+begin
+  LabelP.Caption := IntToStr(TrackBarContrast.Position);
+end;
+
+procedure TFormUtama.AssignBitmapBinary();
+var
+  x, y, threshold, gray : integer;
+begin
+  threshold := TrackBarBinary.Position;
+  for y := 0 to ImageSource.Height-1 do
+  begin
+    for x := 0 to ImageSource.Width-1 do
+    begin
+      gray := (bitmapR[x,y] + bitmapG[x,y] + bitmapB[x,y]) div 3;
+      if gray <= threshold then
+      begin
+        bitmapBinary[x,y] := false;
+      end
+      else
+      begin
+        bitmapBinary[x,y] := true;
+      end;
+    end;
+  end;
+end;
 
 end.
 
