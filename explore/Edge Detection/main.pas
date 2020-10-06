2002811f7eb4c23d3b38c348b38f37908a4db59a
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-07 02:12:54 +0700

    feat: add load and save image feature

--- explore/Edge Detection/main.pas
+++ explore/Edge Detection/main.pas
@@ -5,7 +5,8 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ExtDlgs;
 
 type
 
@@ -15,11 +16,17 @@ type
     ButtonExecute: TButton;
     ButtonSaveImage: TButton;
     ButtonLoadImage: TButton;
-    Image1: TImage;
-    Image2: TImage;
+    ImageSource: TImage;
+    ImageResult: TImage;
+    OpenPictureDialog1: TOpenPictureDialog;
     RadioGroupEdgeDetector: TRadioGroup;
+    SavePictureDialog1: TSavePictureDialog;
+    procedure ButtonLoadImageClick(Sender: TObject);
+    procedure ButtonSaveImageClick(Sender: TObject);
   private
-
+    procedure InitKernel();
+    procedure InitPadding();
+    function Constrain(value: Integer): Byte;
   public
 
   end;
@@ -30,6 +37,51 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+uses
+  windows;
+
+var
+  bitmapR, bitmapG, bitmapB, bitmapGray: array[1..1000, 1..1000] of Byte;
+  imageWidth, imageHeight: Integer;
+
+procedure TForm1.ButtonLoadImageClick(Sender: TObject);
+var
+  x, y: Integer;
+begin
+  if OpenPictureDialog1.Execute then
+  begin
+    ImageSource.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+
+    imageWidth:= ImageSource.Width;
+    imageHeight:= ImageSource.Height;
+
+    ImageResult.Width:= imageWidth;
+    ImageResult.Height:= imageHeight;
+
+    for y:= 0 to imageWidth do
+    begin
+      for x:= 0 to imageHeight do
+      begin
+        bitmapR[x, y]:= GetRValue(ImageSource.Canvas.Pixels[x, y]);
+        bitmapG[x, y]:= GetGValue(ImageSource.Canvas.Pixels[x, y]);
+        bitmapB[x, y]:= GetBValue(ImageSource.Canvas.Pixels[x, y]);
+
+        bitmapGray[x, y]:= (bitmapR[x, y] + bitmapG[x, y] + bitmapB[x, y]) div 3;
+      end;
+    end;
+  end;
+end;
+
+procedure TForm1.ButtonSaveImageClick(Sender: TObject);
+begin
+  if SavePictureDialog1.Execute then
+  begin
+    ImageResult.Picture.SaveToFile(SavePictureDialog1.FileName);
+  end;
+end;
 
 end.
 
